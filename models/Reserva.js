337be66df7e7aebe195f71c695852d716f1a5b36
MaUrO <mauasselborn@gmail.com>
2021-02-28 20:53:59 -0300

    Se crea y configura parcialmete el modelo de reservas

---
+++ models/Reserva.js
@@ -0,0 +1,19 @@
+const { date } = require("joi");
+const { Schema, model } = require("mongoose");
+
+const reservaSchema = Schema({
+    cancha: {
+        type: Number,
+        require: true,
+    },
+    name: {
+        type: String,
+        require: true,
+    },
+    ts_created: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+module.exports = model("reservas", reservaSchema);
